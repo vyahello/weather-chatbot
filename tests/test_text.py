60a7e8ba2b09e5f6e1af86fbbd9be2edaea0e7b0
Volodymyr Yahello <vyahello@gmail.com>
2018-07-30 00:49:54 +0300

    Add unittests for bot input text

---
+++ tests/test_text.py
@@ -0,0 +1,15 @@
+import pytest
+from bin.bot.text import Text, InputText
+
+
+@pytest.fixture(scope='module')
+def input_text() -> Text:
+    return InputText(text='/test', pattern=r'\w+')
+
+
+def test_match(input_text: Text) -> None:
+    assert input_text.match()
+
+
+def test_get(input_text: Text) -> None:
+    assert input_text.get() == 'test'
